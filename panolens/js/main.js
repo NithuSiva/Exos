340187af5de8dda7ee914da32fa0506a55d6ffbf
NithuSiva <47365744+NithuSiva@users.noreply.github.com>
2021-01-05 11:53:01 +0100

    Update main.js

--- panolens/js/main.js
+++ panolens/js/main.js
@@ -1,82 +1,30 @@
-let rects = [];
-let g = [randRect()]; //tableau initiale contenant les coordonnes d'un rectangle.//
+window.addEventListener("deviceorientation", handleOrientation, true);
 
-console.log( document.body.clientWidth, document.body.clientHeight);
-window.addEventListener("load" , main);
-
-function main(event){
-    let n = 100;
-    let t = hitTestAll(g,n);
-    tableau(5);
-    for (let i = 0; i<n; i++){
-	factory(tableau[i], t[i].x, t[i].y, t[i].w, t[i].h);
-    }
+function handleOrientation(event) {
+  var absolute = event.absolute;
+  var alpha    = event.alpha;
+  var beta     = event.beta;
+  var gamma    = event.gamma;
 }
 
-function tableau(n){ //Pour creer plusieurs factory j'avais besoin plusieurs letiable "el" different pour cela j'ai une fonction qui creer un tableau contenant des nombre dans des " " pour les definir comme des charactere. Ex: tableau(5) -> [ "0", "1", "2", "3", "4" ].//
-    let t = [], i = 0, lettre;
-    while(i<n){
-        lettre = i + "";
-    t.push(lettre);
-    i++;
-    }
-    return t;
-}
-   
+let panorama, viewer;
 
-function randRange(min, max){
-    return Math.floor(Math.random() * (max-min+1)) + min;
-}
+panorama = new PANOLENS.ImagePanorama( 'asset/first.jpeg' );
 
-function randomColor(){
-    return "#" + Math.round(Math.random() * 0xFFFFFF).toString(16);
-}
-
-function hitTest(r1, r2){
-    return (((r1.x + r1.w >= r2.x) && (r1.x <= r2.x + r2.w )) && ((r1.y + r1.h >= r2.y) && (r1.y <= r2.y + r2.h)) || (((r2.x + r2.w) > document.body.clientWidth) || ((r2.y + r2.h) > document.body.clientHeight)));
-}
-   
-function hitTestTableau(l,r){ //fonction qui renvoie vrai lorsque qu'il n'ya pas de collision entre les element du tableau et une variable randRect(). //
-    let i = 0;
-    let len = l.length;
-    while(i<len){
-        if ( hitTest(l[i],r) == false){
-            i++;
-        }
-        else {
-            return false;
-        }
-    }
-    return true;
-}
-
-function hitTestAll(l,n){  //fonction qui renvoie un tableau contenant des coordonnes de rectangles qui ne sont jamais en collision.//
-    let i = 0;
-    while(i<n){
-        let r1 = randRect();
-        if (hitTestTableau(l,r1) == true){
-            l.push(r1);
-            i++;
-        } else {
-            i = i;
-        }
-    }
-    return l;
-}
-        
-
-function randRect(){
-    return {x: randRange(0, document.body.clientWidth), y: randRange(0, document.body.clientHeight), w: randRange(10, 200), h: randRange(10, 200)}
-}
-
-function factory(el, x, y, w, h){
-    let element = document.createElement(el);
-    element.style.position = "absolute";
-    element.style.left = x + "px";
-    element.style.top = y + "px";
-    element.style.width = w + "px";
-    element.style.height = h + "px";
-    element.style.backgroundColor = randomColor();
-    document.body.appendChild(element);
-}
-
+viewer = new PANOLENS.Viewer({
+        container: document.body,        // A DOM Element container
+        controlBar: true,             // Vsibility of bottom control bar
+        controlButtons: [],            // Buttons array in the control bar. Default to ['fullscreen', 'setting', 'video']
+        autoHideControlBar: false,        // Auto hide control bar
+        autoHideInfospot: true,            // Auto hide infospots
+        horizontalView: false,            // Allow only horizontal camera control
+        cameraFov: 60,                // Camera field of view in degree
+        reverseDragging: false,            // Reverse orbit control direction
+        enableReticle: false,            // Enable reticle for mouseless interaction
+        dwellTime: 1500,            // Dwell time for reticle selection in millisecond
+        autoReticleSelect: true,        // Auto select a clickable target after dwellTime
+        viewIndicator: false,            // Adds an angle view indicator in upper left corner
+        indicatorSize: 30,            // Size of View Indicator
+        output: 'console'            // Whether and where to output infospot position. Could be 'console' or 'overlay'
+    });
+viewer.add( panorama );
